a7516c214146d3cd437f212bdaad34bb4ae3b381
ayo-ola0710 <israelolatunle2015@gmail.com>
2026-02-14 13:27:33 +0100

    added auth logic

---
+++ api/routes/authRoute.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { register, login } from "../controller/authController.js";
+
+const authRouter = Router();
+
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+export default authRouter;
+
